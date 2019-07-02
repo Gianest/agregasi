7b0fd66888bbb917439430644036c71f600e606c
dsgfresg <dsgdasf@>
2019-07-02 12:08:16 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agregasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawanl = new Karyawan();
+            karyawanl.Nama = "Paijo";
+            Karyawan karyawan2 = new Karyawan();
+            karyawan2.Nama = "Jhono";
+            Perusahaan perusahaan = new Perusahaan();
+            perusahaan.DaftarKaryawan.Add(karyawanl);
+            perusahaan.DaftarKaryawan.Add(karyawan2);
+            int noUrut = 1;
+            Console.WriteLine("Daftar karyawan:");
+            foreach (Karyawan karyawan in perusahaan.DaftarKaryawan)
+            {
+                Console.WriteLine("{0}. {1}", noUrut, karyawan.Nama);
+                noUrut++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ perusahaan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agregasi
+{
+    public class Perusahaan
+    {
+        public List<Karyawan> DaftarKaryawan { get; set; } = new List<Karyawan>();
+    }
+    public class Karyawan
+    {
+            public string Nama{ get; set; }
+    }
+}
